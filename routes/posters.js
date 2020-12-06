d5d8cb2f7d9761055bfade6005dd75f343deb322
Alejandro Avalo <avavalo@falabella.com.ar>
2020-12-06 20:39:23 -0300

    add poster route

--- routes/posters.js
+++ routes/posters.js
@@ -9,7 +9,7 @@ router.get('/', async (req,res)=>{
     res.json('**** Welcome to Digital Posters ****');
 }); 
 
-router.post('/create-map',async (req,res)=>{
+router.post('/poster/create',async (req,res)=>{
     try {
         let { mapSize, bombCount } = req.body;
         let cells = mapController.createMap(mapSize, mapSize);
@@ -25,61 +25,8 @@ router.post('/create-map',async (req,res)=>{
     }
 });
 
-router.post('/selected-cell',async (req,res)=>{
-    let { row, column, selectedCells, value } = req.body.props;
-    let { clicked, flag } = req.map.state;
-    if (!flag) mapController.setState({ clicked: true });
-    if (!clicked) selectedCells();
-    if (!endMineSweeperGame) {
-      // Empty cell click --> recursion
-      if (value === "" && target.id === `${row}_${column}`)
-        mapController.recursionClick(target, row, column);
-      //click bomb scenario --> end game
-      if (value === "☀" && !flag) mapController.endGame(target);
-    }
-});
 
-// track of how many cells are clicked
-router.post('/selected-cells-count',async (req,res)=>{
-    try {
-        let { mapSize, bombCount, cellsClicked } = req.map.state;
-        let safeCells = mapSize * mapSize - bombCount;
-        mapController.setState({
-            cellsClicked: cellsClicked + 1
-        });
-        if (cellsClicked >= safeCells) 
-        res.json({message : constants.messages.WIN_MESSAGE });
-    } catch (error) {
-        res.json({message : error})
-    }
-});
-
-router.post('/selected-cells-count',async (req,res)=>{
-    try {
-        let { mapSize, bombCount, selectedCells } = req.map.state;
-        let safeCells = mapSize * mapSize - bombCount;
-        mapController.setState({
-            selectedCells: selectedCells + 1
-        });
-        if (selectedCells >= safeCells) 
-        res.json({message : constants.messages.WIN_MESSAGE });
-    } catch (error) {
-        res.json({message : error})
-    }
-});
-
-router.post('/update-map',async (req,res)=>{
-    try {
-        let { mapSize, bombCount, selectedCells, cells, _id } = req.body;
-        req.map = await mapController.saveMap(mapSize, bombCount, cells, selectedCells, _id);
-        res.status(201).json(req.map);
-
-    } catch (error) {
-        res.json({message : error})
-    }
-});
-
-router.get('/get-map/:id',async (req,res)=>{
+router.get('/poster/list',async (req,res)=>{
     try {
         let { id } = req.params;
         req.map = await mapController.getMap(id);
@@ -90,6 +37,28 @@ router.get('/get-map/:id',async (req,res)=>{
     }
 });
 
+
+router.get('/poster/:id',async (req,res)=>{
+    try {
+        let { id } = req.params;
+        req.map = await mapController.getMap(id);
+        res.status(201).json(req.map);
+
+    } catch (error) {
+        res.json({message : error})
+    }
+});
+
+router.post('/poster/update',async (req,res)=>{
+    try {
+        let { mapSize, bombCount, selectedCells, cells, _id } = req.body;
+        req.map = await mapController.saveMap(mapSize, bombCount, cells, selectedCells, _id);
+        res.status(201).json(req.map);
+
+    } catch (error) {
+        res.json({message : error})
+    }
+});
 
 
 module.exports = router;
