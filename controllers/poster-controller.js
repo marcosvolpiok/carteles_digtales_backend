5eb958f41eeaea66cfe0888236e761323ade32d8
Marcos Volpi <marcos.volpi@BP4.com>
2020-12-19 03:20:49 -0300

    Quit unnecesary code

--- controllers/poster-controller.js
+++ controllers/poster-controller.js
@@ -1,61 +1,5 @@
 const PosterModel = require('../models/PosterModel');
 
-const createMap = (row, col) => {
-  let outerArray = [];
-  for (let i = 0; i < row; i++) {
-    let innerArray = [];
-    for (let j = 0; j < col; j++) {
-      innerArray.push("");
-    }
-    outerArray.push(innerArray);
-  }
-  return outerArray;
-}
-
-const fillMap = (map, val, count) => {
-  let rows = map.length;
-  let cols = map[0].length;
-  while (count) {
-    let y = floorRand(rows);
-    let x = floorRand(cols);
-    if (!map[y][x]) {
-      map[y][x] = val;
-      count--;
-    }
-  }
-  return map
-};
-
-const floorRand = (scale) => {
-  return Math.floor(Math.random() * scale);
-};
-
-const  adjacentCellsValues = ( map, val) => {
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[0].length; j++) {
-      if (map[i][j] === val) {
-        map = addOneNestedArrAdjacents(map, i, j, val);
-      }
-    }
-  }
-  return map;
-}
-
-const addOneNestedArrAdjacents = ( map, i,j, val) => {
-  let iList = [i - 1, i, i + 1];
-  let jList = [j - 1, j, j + 1];
-  for (let a of iList) {
-    if (map[a]) {
-      for (let b of jList) {
-        if (map[a][b] !== undefined && map[a][b] !== val) {
-          if (typeof map[a][b] !== "number") map[a][b] = 0;
-          map[a][b]++;
-        }
-      }
-    }
-  }
-  return map;
-}
 
 const add = async (data, res) => {
   data['user'] = res.userData.userId;
@@ -95,9 +39,6 @@ const remove = async (id) => {
 
 
 module.exports = {
-  createMap,
-  fillMap,
-  adjacentCellsValues,
   add,
   getPoster,
   getPosterById,
