7ef8b72787e421e3d4ca9c03b0f18a2889d67a7a
Marcos Volpi <marcos.volpi@BP4.com>
2021-01-04 20:10:25 -0300

    Fix

--- helpers/loginHelper.js
+++ helpers/loginHelper.js
@@ -17,14 +17,13 @@ const verifyPassword = (user,req,res)=>{
 }
 
 
-/************* ESTA FUNCIÓN NO SE USA ¿BORRAR? */
 const getToken = (user,res) =>{
     const token = jwt.sign({ email: user.email, userId : user._id,},
         process.env.JWT_KEY, { expiresIn:"1h"})
 
         //return {status: 200, message: 'Auth successful', user, token: token}
         
-    res.json({
+    return res.json({
         message : "Auth successful",
         user,
         token : token
