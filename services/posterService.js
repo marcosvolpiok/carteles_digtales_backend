4fbf454cd5721e0537a4e982cc41f1e3c0242d85
Marcos Volpi <marcos.volpi@BP4.com>
2021-01-30 22:58:33 -0300

    Fix

--- services/posterService.js
+++ services/posterService.js
@@ -24,8 +24,8 @@ class posterService {
 
         this.cloudinary.config({ 
             cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-            api_key: CLOUDINARY_API_KEY, 
-            api_secret: CLOUDINARY_SECRET_KEY
+            api_key: process.env.CLOUDINARY_API_KEY, 
+            api_secret: process.env.CLOUDINARY_SECRET_KEY
         });
 
 
